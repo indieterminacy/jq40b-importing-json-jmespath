80ecb470bc79e1c4ba4b680aa14e492312805189
indieterminacy <indieterminacy@nortonspeal.com>
2016-04-15 12:54:51 +0200

    Moved features abosorbed from 20_Content/20k_Content_Types/60_json-qiuy to disparate Qiuy folder strcuture, after 60o_JSON_Qiuy folders

---
+++ jq40b-import-json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Load external tools
+'''
+import json
+import jmespath
+# import demjson
+# from pprint import pprint
+'''
+Load Json-Qiuy module folders
+Some uncommented, as no rules or content has been introduced yet
+'''
